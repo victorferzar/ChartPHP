ef8d260b8896c2a45aa9aa2c2063e35dffeb2a13
victor.ferzar@gmail.com <viikthor18>
2020-04-04 03:45:06 -0300

    Creacion de Grafico en linea funcional con ingreso de parametros

--- app/Http/Controllers/GeneratePPT.php
+++ app/Http/Controllers/GeneratePPT.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers;
 header('Content-type: application/vnd.openxmlformats-officedocument.presentationml.presentation');
 
+
 use Illuminate\Http\Request;
 use PhpOffice\PhpPresentation\PhpPresentation;
 use PhpOffice\PhpPresentation\IOFactory;
+use PhpOffice\PhpPresentation\Shape\Chart\Type\Line;
+use PhpOffice\PhpPresentation\Style\Border;
 use PhpOffice\PhpPresentation\Style\Color;
-use PhpOffice\PhpPresentation\Style\Alignment;
+use PhpOffice\PhpPresentation\Shape\Chart\Series;
+use PhpOffice\PhpPresentation\Style\Fill;
+use PhpOffice\PhpPresentation\Style\Outline;
+use PhpOffice\PhpPresentation\Style\Shadow;
+
 
 
 class GeneratePPT extends Controller
 {
+
     public function index()
     {
         return view('welcome');
@@ -19,104 +27,104 @@ class GeneratePPT extends Controller
 
     }
 
-//funcion que genera el pptsdsdsa
+//funcion que genera el ppt
     public function generateppt(Request $request)
     {
         //return $request->all();
-        $cantidad = $request->cantidad;
+
+        /*Se rescatan los datos enviados por la vista welcome usando $request
+         * se asignan esos datos a una correspondiente variable
+         * se insertan dichos datos en un array $seriesData
+         * @seriesData = datos a mostrar como puntos en el grafico
+         */
+        $vLunes = (int)$request->dLunes;
+        $vMartes = (int)$request->dMartes;
+        $vMiercoles = (int)$request->dMiercoles;
+        $vJueves = (int)$request->dJueves;
+        $vViernes = (int)$request->dViernes;
+        $vSabado = (int)$request->dSabado;
+        $vDomingo = (int)$request->dDomingo;
+
+        $seriesData = array(
+            'Monday' => $vLunes,
+            'Tuesday' => $vMartes,
+            'Wednesday' => $vMiercoles,
+            'Thursday' => $vJueves,
+            'Friday' => $vViernes,
+            'Saturday' => $vSabado,
+            'Sunday' => $vDomingo
+        );
+
+        /**
+         * se crea una nueva instancia de PowerPoint
+         */
+        $objPPT = new PhpPresentation();
+        // $oMasterSlide = $objPPT->getAllMasterSlides()[0];
+        // $oSlideLayout = $oMasterSlide->getAllSlideLayouts()[0];
+
+        $objPPT->getDocumentProperties()->setCreator('Austem');
+
+        $this->crearSlide($objPPT, $seriesData);
 
 
-        $objPHPPowerPoint = new PhpPresentation();
+        //GUARDAR EN EL EQUIPO
+        $oWriterPPTX = IOFactory::createWriter($objPPT, 'PowerPoint2007');
 
-        $objPHPPowerPoint->getProperties()->setCreator('Austem')
-            ->setLastModifiedBy('Sketch Team')
-            ->setTitle('Prueba de Presentacion')
-            ->setSubject('Prueba de Presentacion')
-            ->setDescription('Generacion automatica de ppts con imagenes')
-            ->setKeywords('office 2007 openxml libreoffice odt php')
-            ->setCategory('Sample Category');
-
-        $objPHPPowerPoint->removeSlideByIndex(0);
-
-        $this->slide1($objPHPPowerPoint, $cantidad);
-     //   $this->slide2($objPHPPowerPoint);
-
-        $oWriterPPTX = IOFactory::createWriter($objPHPPowerPoint, 'PowerPoint2007');
-
-        //return $oWriterPPTX->save(__DIR__ . "/sample.pptx");
         $rutaArchivo = __DIR__ . "/sample.pptx";
-        $oWriterPPTX->save(__DIR__ . "/sample.pptx");
+        $oWriterPPTX->save($rutaArchivo);
         readfile($rutaArchivo);
         exit;
 
     }
 
-    public function slide1(&$objPHPPowerPoint, $cantidad)
+    public function crearSlide(PhpPresentation $objPPT, $seriesData)
     {
-        for ($i = 1; $i <= $cantidad; $i++) {
-            // Create slide
-            $currentSlide = $objPHPPowerPoint->createSlide();
+        /**
+         * Creacion de la diapositiva
+         */
+        $currentSlide = $objPPT->createSlide();
 
-            // Create a shape (drawing)
-            $shape = $currentSlide->createDrawingShape();
-            $shape->setName('imageLeft')
-                ->setDescription('image of the left')
-                ->setPath(public_path() . '/line_graph.jpg')
-                ->setHeight(300)
-                //  ->setWidht(200)
-                ->setOffsetX(10)
-                ->setOffsetY(200);
+        //Llena un objeto Shape de un color solido, en este caso
+        $oFill = new Fill();
+        $oFill->setFillType(Fill::FILL_SOLID)->setStartColor(new Color('ffe6e6'));
+        //Sombra del objeto Shape
+        $oShadow = new Shadow();
+        $oShadow->setVisible(true)->setDirection(45)->setDistance(10);
 
-            $shape = $currentSlide->createDrawingShape();
-            $shape->setName('imageRight')
-                ->setDescription('image of the right')
-                ->setPath(public_path() . '/environmental_data.jpg')
-                ->setHeight(300)
-                //  ->setWidht(200)
-                ->setOffsetX(500)
-                ->setOffsetY(200);
+        //Define Line Chart lines
+        $oOutline = new Outline();
+        $oOutline->getFill()->setFillType(Fill::FILL_SOLID);
+        $oOutline->getFill()->setStartColor(new Color(Color::COLOR_BLACK));
+        $oOutline->setWidth(1);
 
-            //Create a shape (text)
-            $shape = $currentSlide->createRichTextShape()
-                ->setHeight(300)
-                ->setWidth(600)
-                ->setOffsetX(170)
-                ->setOffsetY(180);
-            $shape->getActiveParagraph()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
-            $textRun = $shape->createTextRun('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ');
-            $textRun->getFont()->setBold(true)
-                ->setSize(16)
-                ->setColor(new Color('FFE06B20'));
+        // Create a line chart (that should be inserted in a shape)
+        $lineChart = new Line();
+        $series = new Series('Views', $seriesData);
 
-        }
+        $series->setShowSeriesName(false);
+        $series->setShowValue(true);
+        $series->getFont()->setSize(12);
+        $series->setOutline($oOutline);
+
+        $lineChart->addSeries($series);
+        /**
+         * Creacion del Shape (forma)
+         * Shape = objetos que pueden ser agregados a una diapositiva
+         */
+        $shape = $currentSlide->createChartShape();
+        $shape->setName('PHPPRESENTATION PRUEBA DE GRAFICO')->setResizeProportional(false)->setHeight(550)->setWidth(700)->setOffsetX(120)->setOffsetY(80);
+        $shape->setShadow($oShadow);
+        $shape->setFill($oFill);
+        $shape->getBorder()->setLineStyle(Border::LINE_SINGLE);
+        $shape->getTitle()->setText('PHPPRESENTATION PRUEBA DE GRAFICO');
+        $shape->getTitle()->getFont()->setItalic(true);
+        $shape->getPlotArea()->setType($lineChart);
+        $shape->getView3D()->setRotationX(30);
+        $shape->getView3D()->setPerspective(30);
+        $shape->getLegend()->getBorder()->setLineStyle(Border::LINE_SINGLE);
+        $shape->getLegend()->getFont()->setItalic(true);
+
     }
-//    public function slide2(&$objPHPPowerPoint)
-//    {
-//
-//        // Create slide
-//        $currentSlide = $objPHPPowerPoint->createSlide();
-//
-//        // Create a shape (drawing)
-//        $shape = $currentSlide->createDrawingShape();
-//        $shape->setName('image')
-//            ->setDescription('image')
-//            ->setPath(public_path() . '/environmental_data.jpg')
-//            ->setHeight(300)
-//            ->setOffsetX(10)
-//            ->setOffsetY(10);
-//
-//        // Create a shape (text)
-//        $shape = $currentSlide->createRichTextShape()
-//            ->setHeight(300)
-//            ->setWidth(600)
-//            ->setOffsetX(170)
-//            ->setOffsetY(180);
-//        $shape->getActiveParagraph()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
-//        $textRun = $shape->createTextRun('Lorem Ipsum is simply dummy text of the printing and typesetting industry.');
-//        $textRun->getFont()->setBold(true)
-//            ->setSize(16)
-//            ->setColor(new Color('FFE06B20'));
-//
-//    }
+
 }
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,11 +13,30 @@
 </head>
 <body>
 
-<form action="{{route('generar')}}">
+<form action="{{route('generar')}}" class="form-group">
     <h1>Generador de PPT</h1>
-    <input type="number" name="cantidad" required placeholder="Cantidad de diapositivas" class="form-control mb-2" min="1"
-           max="10">
+
+    <div class="form-inline">
+        <input type="date" name="fecha_des" required placeholder="Fecha Desde" value="1991-09-13" disabled>
+        <input type="date" name="fecha_has" required placeholder="Fecha Hasta" value="2020-04-03" disabled>
+    </div>
+    <div class="form-inline">
+        <input type="number" name="dLunes" placeholder="Lunes: ">
+        <input type="number" name="dMartes" placeholder="Martes: ">
+        <input type="number" name="dMiercoles" placeholder="Miercoles: ">
+        <input type="number" name="dJueves" placeholder="Jueves: ">
+        <input type="number" name="dViernes" placeholder="Viernes: ">
+        <input type="number" name="dSabado" placeholder="Sabado: ">
+        <input type="number" name="dDomingo" placeholder="Domingo: ">
+
+
+    </div>
+
+    <input type="number" name="cantidad" required placeholder="Cantidad de diapositivas" class="form-control mb-2"
+           min="1"
+           max="10" disabled>
     <button type="submit" class="btn btn-primary btn-block">GENERAR</button>
+
 </form>
 
 <!-- Optional JavaScript -->
