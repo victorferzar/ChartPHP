5c23ee66c9f1ba2b48701c705457962c66d88654
victor.ferzar@gmail.com <viikthor18>
2020-04-15 12:47:23 -0400

    Elementos re organizados visualmente, agregado logo tipo

--- app/Http/Controllers/GeneratePPT.php
+++ app/Http/Controllers/GeneratePPT.php
@@ -7,284 +7,264 @@ header('Content-type: application/vnd.openxmlformats-officedocument.presentation
 
 use DateTime;
 use Illuminate\Http\Request;
-use phpDocumentor\Reflection\Types\Array_;
-use PhpOffice\PhpPresentation\Shape\RichText\Run;
+
+use Illuminate\Support\Facades\Auth;
 use PhpOffice\PhpPresentation\PhpPresentation;
 use PhpOffice\PhpPresentation\IOFactory;
-use PhpOffice\PhpPresentation\Shape\Chart\Type\Line;
-use PhpOffice\PhpPresentation\Shape\Chart\Type\Scatter;
-use PhpOffice\PhpPresentation\Shape\RichText;
+use PhpOffice\PhpPresentation\Shape\Chart\Axis;
+use PhpOffice\PhpPresentation\Shape\Chart\Gridlines;
+use PhpOffice\PhpPresentation\Shape\Table\Cell;
 use PhpOffice\PhpPresentation\Style\Border;
 use PhpOffice\PhpPresentation\Style\Color;
 use PhpOffice\PhpPresentation\Shape\Chart\Series;
 use PhpOffice\PhpPresentation\Style\Fill;
-use PhpOffice\PhpPresentation\Style\Outline;
-use PhpOffice\PhpPresentation\Style\Shadow;
-use PhpOffice\PhpPresentation\Shape\Table;
-use PhpOffice\PhpPresentation\Shape\Table\Row;
-use PhpOffice\PhpPresentation\Shape\Table\Cell;
-use PhpOffice\PhpPresentation\Shape\Placeholder;
 use PhpOffice\PhpPresentation\Shape\Chart\Type\Bar;
 use PhpOffice\PhpPresentation\Style\Alignment;
+use PhpOffice\PhpPresentation\Shape\Chart\Type\Line;
+
 use App;
+use PhpOffice\PhpPresentation\Style\Outline;
+
 
 class GeneratePPT extends Controller
 {
 
+    public const Datos_Filtro_Blancos_AssayName = array(
+        1 => "CuT_CMCCAAS_pct",
+        2 => "CuT_pct_2A15tAA",
+        3 => "CuS_CMCCAAS_pct",
+        4 => "CuS_pct_SCA2pAA",
+        5 => "CuCN_pct_CYN6pAA",
+        6 => "CuFe_pct_FES7pAA"
+    );
+
+    public const Datos_Filtro_Blancos_StandardId = array(
+        1 => "BF42",
+        2 => "BG4"
+    );
+
+    public const Datos_Tabla = array(
+        1 => "# of Analyses Above Threshold",
+        2 => "# of Outside Warning Limit",
+        3 => "# of Outside Error Limit",
+        4 => "# of Analyses Bellow Threshold",
+        5 => "% Outside Error Limit",
+        6 => "Mean",
+        7 => "Median",
+        8 => "Min",
+        9 => "Max",
+        10 => "Standard Deviation",
+        11 => "Rel. Std. Dev",
+        12 => "Standard Error",
+        13 => "Rel. Std. Err",
+        14 => "Total Bias",
+        15 => "% Mean Bias"
+    );
 
     public function index()
     {
         return view('welcome');
     }
 
-    public function filtrarBlancos($standardID, $assayName, $vDesde, $vHasta)
+    public function filtro($standardID, $assayName, $vDesde, $vHasta)
     {
-
-        $datosBD = App\Dtm_qaqc_blk_std::select("*")
+        $datosBD = App\Dtm_qaqc_blk_std::selectRaw("TRY_CONVERT(date, RETURNDATE,103) as FECHAS, SUM(ASSAYVALUE) as SUMA")
             ->where("STANDARDID", "=", "$standardID")
             ->where("ASSAYNAME", "=", "$assayName")
             ->whereRaw("TRY_CONVERT(date, RETURNDATE, 103) Between ('$vDesde') AND ('$vHasta')")
-            ->orderByRaw('TRY_CONVERT(date, RETURNDATE, 103) Asc')
+            ->groupByRaw("TRY_CONVERT(date, RETURNDATE, 103)")
+            //->orderByRaw('TRY_CONVERT(date, RETURNDATE, 103) DESC')
             ->get();
-        return $datosBD;
 
+        return $datosBD;
     }
 
     public function generateppt(Request $request)
     {
-
         //return $request->all();
+        //RECIBE RANGO DE FECHAS DEL FORMULARIO
         $vDesde = $request->input("fecha_des");
         $vHasta = $request->input("fecha_has");
-
-        // $datosBD = $this->filtrarBlancos("BF42", "CuT_CMCCAAS_pct", $vDesde, $vHasta);
-
-        /*   $datosBD = App\Dtm_qaqc_blk_std::select("*")
-               ->where("STANDARDID","=","BF42")
-               ->where("ASSAYNAME","=","CuT_CMCCAAS_pct")
-               ->whereRaw("TRY_CONVERT(date, RETURNDATE, 103) Between ('$vDesde') AND ('$vHasta')")
-               ->get();
-
-   */
-
-        //return $datosBD->count();
-
 
         /**
          * se crea una nueva instancia de PowerPoint
          */
         $objPPT = new PhpPresentation();
-        $objPPT->getDocumentProperties()->setCreator('Austem');
+        $objPPT->getDocumentProperties()
+            ->setCreator('Austem');
 
-        /*  $dataBD = App\Dtm_qaqc_blk_std::where('STANDARDID', 'BF40')
-              ->where('ASSAYNAME', 'CuT_CMCCAAS_pct')
-              ->get();
-  */
-        //DATOS DEL GRAFICO
-
+        //DIBUJAR GRAFICO BLANCOS
         $this->blankDraw($objPPT, $vDesde, $vHasta);
 
 
         //GUARDAR EN EL EQUIPO
         $oWriterPPTX = IOFactory::createWriter($objPPT, 'PowerPoint2007');
-
         $rutaArchivo = __DIR__ . "/sample.pptx";
         $oWriterPPTX->save($rutaArchivo);
         readfile($rutaArchivo);
         exit;
     }
 
-
-    /***
-     * CREA LA DIAPOSITIVA Y ESTABLECE EL FONDO DEL OBJETO Y LA SOMBRA QUE DESPERENDE
-     * @param PhpPresentation $objPPT ducumento ppt
-     * @param $seriesData  valores del grafico
-     * @throws \Exception     *
-     */
-    public function crearSlide(PhpPresentation $objPPT, $seriesData)
-    {
-
-        //Llena un objeto Shape de un color solido, en este caso
-        $oFill = new Fill();
-        $oFill->setFillType(Fill::FILL_SOLID)->setStartColor(new Color('ffe6e6'));
-
-        //Sombra del objeto Shape
-        $oShadow = new Shadow();
-        $oShadow->setVisible(true)->setDirection(45)->setDistance(10);
-
-        //Define Line Chart lines
-        $oOutline = new Outline();
-        $oOutline->getFill()->setFillType(Fill::FILL_SOLID);
-        $oOutline->getFill()->setStartColor(new Color(Color::COLOR_BLACK));
-        $oOutline->setWidth(1);
-
-        // Create a line chart (that should be inserted in a shapeLeft)
-        $lineChart = new Line();
-        $series = new Series('Views', $seriesData);
-
-        $series->setShowSeriesName(false);
-        $series->setShowValue(true);
-        $series->getFont()->setSize(12);
-        $series->setOutline($oOutline);
-
-        $lineChart->addSeries($series);
-
-        //  $this->chartLeft($currentSlide, $oFill, $oShadow, $lineChart);
-        // $this->chartRight($currentSlide, $oFill, $oShadow, $seriesData);
-    }
-
-    public function chartLeft($currentSlide, $oFill, $oShadow, $lineChart)
-    {
-        $shapeLeft = $currentSlide->createChartShape();
-        $shapeLeft->setName('PHPPRESENTATION DE LA IZQUIERDA')
-            ->setResizeProportional(false)
-            ->setHeight(400)
-            ->setWidth(450)
-            ->setOffsetX(10)
-            ->setOffsetY(120);
-        $shapeLeft->setShadow($oShadow);
-        $shapeLeft->setFill($oFill);
-        $shapeLeft->getBorder()->setLineStyle(Border::LINE_SINGLE);
-        $shapeLeft->getTitle()->setText('PHPPRESENTATION DE LA IZQUIERDA');
-        $shapeLeft->getTitle()->getFont()->setItalic(true);
-        $shapeLeft->getPlotArea()->setType($lineChart);
-        $shapeLeft->getView3D()->setRotationX(30);
-        $shapeLeft->getView3D()->setPerspective(30);
-        $shapeLeft->getLegend()->getBorder()->setLineStyle(Border::LINE_SINGLE);
-        $shapeLeft->getLegend()->getFont()->setItalic(true);
-    }
-
-    public function chartRight($currentSlide, $oFill, $oShadow, $seriesData)
-    {
-        $lineChart = new Scatter();
-        $series = new Series('Valores', $seriesData);
-        $series->setShowSeriesName(true);
-        $series->getMarker()->setSymbol(\PhpOffice\PhpPresentation\Shape\Chart\Marker::SYMBOL_CIRCLE);
-        $series->getMarker()->setSize(10);
-        $lineChart->addSeries($series);
-
-        $shapeRight = $currentSlide->createChartShape();
-        $shapeRight->setName('PHPPRESENTATION DE LA DERECHA')->setResizeProportional(false)->setHeight(400)->setWidth(450)->setOffsetX(500)->setOffsetY(120);
-        $shapeRight->setShadow($oShadow);
-        $shapeRight->setFill($oFill);
-        //$shapeRight->getBorder()->setLineStyle(Border::LINE_SINGLE);
-        $shapeRight->getTitle()->setText('PHPPRESENTATION DE LA DERECHA');
-        $shapeRight->getTitle()->getFont()->setItalic(true);
-        $shapeRight->getPlotArea()->setType($lineChart);
-        $shapeRight->getView3D()->setRotationX(30);
-        $shapeRight->getView3D()->setPerspective(30);
-        $shapeRight->getLegend()->getBorder()->setLineStyle(Border::LINE_SINGLE);
-        $shapeRight->getLegend()->getFont()->setItalic(true);
-    }
-
     public function blankDraw(PhpPresentation $objPPT, $vDesde, $vHasta)
     {
-        //FILTRO BD
-        $dataBD = $this->filtrarBlancos("BF42", "CuT_CMCCAAS_pct", $vDesde, $vHasta);
+        foreach (self::Datos_Filtro_Blancos_StandardId as $vId) {
+            foreach (self::Datos_Filtro_Blancos_AssayName as $vAssay) {
 
-        //DATOS DEL GRAFICO
+                $currentSlide = $objPPT->createSlide();
 
-        $cont = 0;
-        foreach ($dataBD as $num) {
-            $aux = date('d-m', strtotime($num->RETURNDATE));
-            $series1Data[$aux] = floatval($num->ASSAYVALUE);
-            $vId = $num->STANDARDID;
-            $vNombreMuestra = $num->ASSAYNAME;
+                //FILTRO BD
+                $dataBD = $this->filtro($vId, $vAssay, $vDesde, $vHasta);
+                $seriesData = [];
+
+                //DATOS DEL GRAFICO
+                foreach ($dataBD as $num) {
+
+                    if (count($seriesData) == 0) {
+                        $desdeReal = date('d-m-y', strtotime($num->FECHAS));
+                    }
+                    $aux = date('d-m', strtotime($num->FECHAS));
+                    $seriesData[$aux] = floatval($num->SUMA);
+                    $hastaReal = date('d-m-y', strtotime($num->FECHAS));
+
+                }
+
+                //DATOS DE LA TABLA
+                $dataA = self::Datos_Tabla;
+
+                //TABLA
+                $tableShape = $currentSlide->createTableShape(2);
+                $tableShape->setResizeProportional(false)
+                    // ->setHeight(200)
+                    ->setWidth(330)
+                    ->setOffsetX(730)
+                    ->setOffsetY(200);
+
+                $row0 = $tableShape->createRow()
+                    ->setHeight(25);
+
+                $cell00 = $row0->nextCell();
+                $cell00->CreateTextRun("STATISTICS");
+                $cell00->getActiveParagraph()
+                    ->getAlignment()
+                    ->setHorizontal(Alignment::HORIZONTAL_CENTER)
+                    ->setVertical(Alignment::VERTICAL_CENTER);
+
+                $cell01 = $row0->getCell(1);
+                $cell01->createTextRun("d " . strval($dataBD->count()));
+                $cell01->setWidth(60);
+                $cell01->getActiveParagraph()
+                    ->getAlignment()
+                    ->setHorizontal(Alignment::HORIZONTAL_CENTER)
+                    ->setVertical(Alignment::VERTICAL_CENTER);
+
+                for ($i = 1; $i <= 15; $i++) {
+                    $row[$i] = $tableShape->createRow()
+                        ->setHeight(32);
+                    $cellAux = $row[$i]->getCell(0);
+                    $cellAux->createTextRun($dataA[$i]);
+                    $cellAux->getActiveParagraph()
+                        ->getAlignment()
+                        ->setVertical(Alignment::VERTICAL_CENTER)
+                        ->setMarginLeft(2);
+                }
+
+                //TITULO
+                if ($vId == "BF42") {
+                    $vTipo = "Fino";
+                } elseif ($vId == "BG4") {
+                    $vTipo = "Grueso";
+                }
+                //LOGO
+                $logoImg = $currentSlide->createDrawingShape();
+                $logoImg->setName('logo')
+                    ->setDescription('Logotipo Corporativa');
+                $logoImg->setPath(resource_path() . '/images/logo-bhp.png');
+                $logoImg->setOffsetX(60)
+                    ->setOffsetY(60);
+                $logoImg->setResizeProportional(true)
+                    ->setHeight(60);
+
+                //TEXTO
+                $txtTitulo = $currentSlide->createRichTextShape()
+                    ->setHeight(100)
+                    ->setWidth(700)
+                    ->setOffsetX(200)
+                    ->setOffsetY(30);
+
+                $txtTitulo->getActiveParagraph()
+                    ->getAlignment()
+                    ->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+                $txtSubTitulo = $txtTitulo;
+                $txtDetalle = $txtTitulo;
+
+
+                $txtRunT = $txtTitulo->createTextRun("Blancos - " . $vAssay . "\n");
+                $txtRunST = $txtSubTitulo->createTextRun("Blancos Aprobados " . $vId . " - " . "Blanco " . $vTipo . "\n");
+                $txtRunD = $txtDetalle->createTextRun("Estandares desde: " . $desdeReal . " Hasta: " . $hastaReal . " STD: " . $vId . " Elementos: " . $vAssay);
+                $txtRunT->getFont()->setBold(true)
+                    ->setSize(35)
+                    ->setColor(new Color('FFE06B20'));
+                $txtRunST->getFont()->setItalic(true)
+                    ->setSize(25);
+                $txtRunD->getFont()->setSize(12);
+
+
+                $oOutLine = new Outline();
+                $oOutLine->setWidth(1);
+                $oOutLine->getFill()->setFillType(Fill::FILL_SOLID)->setStartColor(new Color(Color::COLOR_BLUE));
+
+                //GRAFICO DE BARRAS
+                //BARRA
+                $barChart = new Bar();
+                // $barChart->setGapWidthPercent(158);
+                $series1 = new Series('VALORES', $seriesData);
+                $series1->setShowSeriesName(false);
+                $series1->getFill()->setFillType(Fill::FILL_SOLID)->setStartColor(new Color('FFA500'));
+                $series1->getFont()->getColor()->setRGB('00FF00');
+                $series1->setShowValue(false);
+                $series1->setOutline($oOutLine);
+                $barChart->addSeries($series1);
+
+                //LINEAS
+                $errLine = new Line();
+                $warLine = new Line();
+
+
+                //GRAFICO
+                $oGrid = new Gridlines();
+                $oGrid->getOutline()->setWidth(1);
+
+                $oGrid->getOutline()->getFill()->setFillType(Fill::FILL_SOLID)->setStartColor(new Color(Color::COLOR_BLUE));
+
+
+                $chartShape = $currentSlide->createChartShape();
+                $chartShape->setName("Grafico de Blancos")->setResizeProportional(false)
+                    ->setHeight(400)
+                    ->setWidth(700)
+                    ->setOffsetX(10)
+                    ->setOffsetY(200);
+                $chartShape->getBorder()->setLineStyle(Border::LINE_SINGLE);
+                $chartShape->getTitle()->setText($vId . " - " . $vAssay);
+                $chartShape->getTitle()->getFont()->setItalic(true);
+                $chartShape->getTitle()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
+                $chartShape->getPlotArea()->getAxisX()->setTitle('Fecha de Retorno');
+                $chartShape->getPlotArea()->getAxisY()->setTitle('Ley Laboratorio');
+                $chartShape->getPlotArea()->getAxisY()->setMajorGridlines($oGrid);
+                //  $chartShape->getPlotArea()->getAxisY()->setFormatCode('#,##0');
+
+
+                //SALTOS ENTRE VALORES EN Y
+                //$chartShape->getPlotArea()->getAxisY()->setMajorUnit(0.002);
+                //VALOR MAXIMO PARA Y
+                //$chartShape->getPlotArea()->getAxisY()->setMaxBounds(0.1);
+                //$chartShape->getPlotArea()->getAxisY()->setMinorTickMark(Axis::TICK_MARK_CROSS);
+
+                $chartShape->getPlotArea()->setType($barChart);
+                $chartShape->getLegend()->setVisible(false);
+            }
         }
-
-        //DATOS DE LA TABLA
-
-        $dataA = array(
-            1 => "# of Analyses Above Threshold",
-            2 => "# of Outside Warning Limit",
-            3 => "# of Outside Error Limit",
-            4 => "# of Analyses Bellow Threshold",
-            5 => "% Outside Error Limit",
-            6 => "Mean",
-            7 => "Median",
-            8 => "Min",
-            9 => "Max",
-            10 => "Standard Deviation",
-            11 => "Rel. Std. Dev",
-            12 => "Standard Error",
-            13 => "Rel. Std. Err",
-            14 => "Total Bias",
-            15 => "% Mean Bias"
-        );
-        //TABLA
-        $currentSlide = $objPPT->createSlide();
-        $tableShape = $currentSlide->createTableShape(2);
-
-        $tableShape->setResizeProportional(false)->setHeight(200)->setWidth(250)->setOffsetX(700)->setOffsetY(100);
-
-        $row0 = $tableShape->createRow();
-
-        $cell00 = $row0->nextCell();
-        $cell00->CreateTextRun("STATISTICS");
-
-        $cell01 = $row0->getCell(1);
-        $cell01->createTextRun(strval($dataBD->count()));
-        $cell01->setWidth(50);
-
-        for ($i = 1; $i <= 15; $i++) {
-            $row[$i] = $tableShape->createRow();
-
-            $cellAux = $row[$i]->getCell(0);
-            $cellAux->CreateTextRun($dataA[$i]);
-        }
-
-        //TITULO
-        $txtShape = $currentSlide->createRichTextShape()->setHeight(100)->setWidth(300)->setOffsetX(10)->setOffsetY(10);
-        $txtShape->getActiveParagraph()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
-        $txtRun = $txtShape->createTextRun("FECHA DESDE: " . date('d-m-y', strtotime($vDesde)) . "\n" . "FECHA HASTA: " . date('d-m-y', strtotime($vHasta)));
-
-        $txtRun->getFont()->setItalic(true)
-            ->setSize(12)
-            ->setColor(new Color('FFE06B20'));
-
-
-        //GRAFICO DE BARRAS
-        //BARRA
-        $barChart = new Bar();
-        $barChart->setGapWidthPercent(158);
-        $series1 = new Series('2009', $series1Data);
-        $series1->setShowSeriesName(false);
-        $series1->getFill()->setFillType(Fill::FILL_SOLID)->setStartColor(new Color('FF4F81BD'));
-        $series1->getFont()->getColor()->setRGB('00FF00');
-        $series1->setShowValue(false);
-        $series1->getDataPointFill(2)->setFillType(Fill::FILL_SOLID)->setStartColor(new Color('FFE06B20'));
-        $barChart->addSeries($series1);
-
-        //GRAFICO
-        $chartShape = $currentSlide->createChartShape();
-        $chartShape->setName("Grafico de Blancos")->setResizeProportional(false)->setHeight(400)
-            ->setWidth(650)
-            ->setOffsetX(20)
-            ->setOffsetY(120);
-        $chartShape->getBorder()->setLineStyle(Border::LINE_SINGLE);
-        $chartShape->getTitle()->setText($vId." - ".$vNombreMuestra);
-        $chartShape->getTitle()->getFont()->setItalic(true);
-        $chartShape->getTitle()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
-        $chartShape->getPlotArea()->getAxisX()->setTitle('Fecha de Retorno');
-        $chartShape->getPlotArea()->getAxisY()->getFont()->getColor()->setRGB('00FF00');
-        $chartShape->getPlotArea()->getAxisY()->setTitle('Ley Laboratorio');
-
-        $chartShape->getPlotArea()->setType($barChart);
-        $chartShape->getLegend()->setVisible(false);
 
     }
 
-    public function buscar(Request $request)
-    {
-        $vDesde = $request->input("fecha_des");
-        $vHasta = $request->input("fecha_has");
-
-        $datosBD = App\Dtm_qaqc_blk_std::where('RETURNDATE', '>=', $vDesde)
-            ->where('RETURNDATE', '=', $vHasta);
-
-        return $request->all();
-
-
-    }
 }
-
